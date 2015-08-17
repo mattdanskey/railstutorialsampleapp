84015ab8d990ddb9fc07b02ad30baf01a26392d4
Matthew Danskey <mdanskey@gmail.com>
2015-08-16 20:52:23 -0700

    set up guard; make tests a little DRYer

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # doesn't need to be declared before every test, as it isn't altered
+  # and therefore doesn't need to be in a setup method
+  base_title = "Ruby on Rails Tutorial Sample App"
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{base_title}"
   end
 end
